f421f1380cac9bb7cc38c512a2fe938f44603961
Thiago <thiagobarroso11@hotmail.com>
2025-03-02 20:29:28 -0300

    tests

--- main_save.py
+++ main_save.py
@@ -57,7 +57,7 @@ if __name__ == '__main__':
 
     dir_base = "./results/new/tishby/12-10-7-5-4-3-2-1/"
     # dir_base = "./results/new/tishby/12-10-8-6-4-2-1/"
-    # dir_base = "./results/new/mnist/784-8-8-8-10"
+    # dir_base = "./results/new/mnist/784-8-8-8-10/"
 
     iterations = 30
 
