217106172ff6cc5561a376436c1b5df986d7b66b
Thiago <thiagobarroso11@hotmail.com>
2025-03-18 20:13:37 -0300

    tests

--- main_save.py
+++ main_save.py
@@ -55,8 +55,8 @@ act_list = [] # [epoch][layer][sample][neuron]
 if __name__ == '__main__':
     os.system('cls')
 
-    dir_base = "./results/new/tishby/12-10-7-5-4-3-2-1/"
-    # dir_base = "./results/new/tishby/12-10-8-6-4-2-1/"
+    # dir_base = "./results/new/tishby/12-10-7-5-4-3-2-1/"
+    dir_base = "./results/new/tishby/12-10-8-6-4-2-1/"
     # dir_base = "./results/new/mnist/784-8-8-8-10/"
 
     iterations = 30
